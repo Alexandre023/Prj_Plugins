13b8b7ef9f3f3661d576b9c25fc9390b2f834b2a
Alexandre023 <alexandre.vieira0203@gmail.com>
2020-05-08 22:52:32 +0900

    Confirm adicionado

--- www/components/plugin.js
+++ www/components/plugin.js
@@ -1,3 +1,15 @@
 $(document).on("click", "#alerta", function(){
   navigator.notification.alert("Minha mensagem",null,"Aviso!","Sim");
 });
+
+$(document).on("click", "#confirm", function(){
+  function confirma(buttonIndex){
+    if(buttonIndex == 1){
+      navigator.notification.alert("Você escolheu a opção A",null,"Aviso!","Ok");
+    }else{
+      navigator.notification.alert("Você escolheu a opção B",null,"Aviso!","Ok");
+    }
+  }
+
+  navigator.notification.confirm("Escolha A ou B",confirma,"Escolha:",['A','B']);
+});
